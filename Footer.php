425ac20536eddf8058a59d0ec4f2999fce43b017
bablyraj <priyagupta82444@gmail.com>
2022-05-26 00:01:05 +0530

    implemented menubar and footer

---
+++ Footer.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <meta name="keywords" content="footer, address, phone, icons" />
+ <title>Gobooc</title>
+ <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+ <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+ <link rel="stylesheet" href="css/footer.css">
+</head>
+    <body>
+        <footer class="footer-distributed">
+            <div class="footer-content">
+                <div class="footer-left">
+                    <h3>Gobooc</h3>
+                    <p class="footer-links">
+                        <a href="index.php">Home</a>
+                        ·
+                        <a href="about.php">About</a>
+                        ·
+                        <a href="gallery.php">Gallery</a>
+                    </p>
+                    <p class="footer-company-name">Gobooc &copy; 2022</p>
+                </div>
+                <div class="footer-center">  
+                    <div>
+                        <i class="fa fa-map-marker"></i>
+                        <p>Bangalore, India</p>
+                    </div>
+                    <div>
+                        <i class="fa fa-phone"></i>
+                        <p>+91 98765 43210</p>
+                    </div>
+                    <div>
+                        <i class="fa fa-envelope"></i>
+                        <p><a href="mailto:support@company.com">contact@gobooc.com</a></p>
+                    </div>
+                </div>
+                <div class="footer-right">
+                    <p class="footer-company-about">
+                    <span>About the company</span>
+                    Gobooc is an establishment that provides paid lodging on a short-term basis. Facilities provided may range from a modest-quality mattress in a small room to large suites.
+                    </p>   
+                    <div class="footer-icons">
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-linkedin"></i></a>
+                        <a href="#"><i class="fa fa-github"></i></a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </body> 
+</html>
