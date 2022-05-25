a338732308191579db1e7ef6c1d3c01020bc01f1
bablyraj <priyagupta82444@gmail.com>
2022-05-26 00:02:14 +0530

    implemented gallery page

---
+++ gallery.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Gobooc</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link href="css/gallery.css"rel="stylesheet"/>
+</head>
+<body style="margin-top:50px;"> 
+  <?php
+    include('Menu Bar.php')
+  ?>
+<div class="container">
+  <div class="gallery">
+    <div class="image">
+      <img src="image/gallery/hotel1.jpg" alt="hotel1" />
+    </div>
+    <div class="image">
+      <img src="image/gallery/hotel2.jpg" alt="hotel2" />
+    </div>
+    <div class="image">
+      <img src="image/gallery/hotel3.jpg" alt="hotel3" />
+    </div>
+    <div class="image">
+      <img src="image/gallery/hotel4.jpg" alt="hotel4" />
+    </div>
+    <div class="image">
+      <img src="image/gallery/hotel5.jpg" alt="hotel5" />
+    </div>
+    <div class="image">
+      <img src="image/gallery/hotel6.jpg" alt="hotel6" />
+    </div>
+    <div class="image">
+      <img src="image/gallery/room.jpg" alt="room" />
+    </div>
+    <div class="image">
+      <img src="image/gallery/room1.jpg" alt="room1" />
+    </div>
+    <div class="image">
+      <img src="image/gallery/room2.jpg" alt="room2" />
+    </div>
+    <div class="image">
+      <img src="image/gallery/room3.jpg" alt="room3" />
+    </div>
+    <div class="image">
+      <img src="image/gallery/room4.jpg" alt="room4" />
+    </div>
+    <div class="image">
+      <img src="image/gallery/room5.jpg" alt="room5" />
+    </div>
+  </div>
+</div>
+<?php
+  include('Footer.php')
+?>
+</body>
+</html>
+
+
